20aa2b01534c8c0c42972646576223a8507ce663
yehzhang <simonzhangintp@gmail.com>
2017-09-28 15:53:15 -0700

    Refactor test utils.

--- test/snapshot.test.js
+++ test/snapshot.test.js
@@ -1,6 +1,11 @@
 import * as babel from 'babel-core';
 import plugin from '../src/index';
-import {testFixtures} from './util';
+import {onFixtures} from './util';
+
+const HIGHER_ORDER_FUNCTION_NAMES = [
+  'forEach',
+  'map',
+];
 
 describe('Match snapshots of generated code for', () => {
   testFixtures((sourceCode) => {
@@ -10,3 +15,14 @@ describe('Match snapshots of generated code for', () => {
     expect(output.code + '\n').toMatchSnapshot();
   });
 });
+
+function testFixtures(f) {
+  onFixtures((fixtureName, sourceCode) => {
+    HIGHER_ORDER_FUNCTION_NAMES.forEach(functionName => {
+      test(`${functionName}, ${fixtureName}`, () => {
+        let sourceCodeInstance = sourceCode.replace(/\bFUN\b/g, functionName);
+        f(sourceCodeInstance);
+      });
+    });
+  });
+}
--- test/util.js
+++ test/util.js
@@ -1,10 +1,6 @@
 import {readdirSync, readFileSync} from 'fs';
 
 const FIXTURES_PATH = 'test/fixtures';
-const HIGHER_ORDER_FUNCTION_NAMES = [
-  'forEach',
-  'map',
-];
 
 export function onFixtures(f) {
   readdirSync(FIXTURES_PATH)
@@ -14,15 +10,4 @@ export function onFixtures(f) {
         let sourceCode = readFileSync(fixturePath).toString();
         f(fixtureFile, sourceCode);
       });
-}
-
-export function testFixtures(f) {
-  onFixtures((fixtureName, sourceCode) => {
-    HIGHER_ORDER_FUNCTION_NAMES.forEach(functionName => {
-      test(`${functionName}, ${fixtureName}`, () => {
-        let sourceCodeInstance = sourceCode.replace(/\bFUN\b/g, functionName);
-        f(sourceCodeInstance);
-      });
-    });
-  });
 }
